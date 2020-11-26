f8bbd5d43ff831e67f4be7ebd1434861d9c59532
Agungsans28 <72182205+Agungsans28@users.noreply.github.com>
2020-11-26 19:28:11 +0800

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'input.dart';
+
+void main() => runApp(MaterialApp(
+  theme: ThemeData(
+    primaryColor: Colors.blue,
+  ),
+  home: input_data(),
+));
+
